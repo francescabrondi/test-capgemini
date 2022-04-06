2cd4127f60aba962bcd8abc4f2631088d4b4ba3f
francescabrondi <37108063+francescabrondi@users.noreply.github.com>
2022-04-06 10:57:55 +0200

    Context cerca immagini

--- src/contexts/ImmaginiContext.js
+++ src/contexts/ImmaginiContext.js
@@ -1,10 +1,12 @@
-import React, {createContext, useState, useEffect} from 'react'
+import React, { createContext, useState, useEffect } from "react";
 
 export const ImagesContext = createContext();
 
 const ImagesContextProvider = (props) => {
-    const [photos, setPhotos] = useState([]);
-    const client_id = process.env.REACT_APP_CLIENT_ID;
+  const [photos, setPhotos] = useState([]);
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const client_id = process.env.REACT_APP_CLIENT_ID;
 
   useEffect(() => {
     fetch(`https://api.unsplash.com/photos/?client_id=${client_id}`, {})
@@ -12,13 +14,29 @@ const ImagesContextProvider = (props) => {
       .then((result) => {
         setPhotos(result);
       });
-  }, []); 
+  }, []);
+
+  useEffect(() => {
+    query &&
+      fetch(
+        `https://api.unsplash.com/search/photos/?client_id=${client_id}&query=${query}`,
+        {}
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setImages(result.results);
+        });
+  }, [query]);
+
+  const SearchImages = (e) => {
+    setQuery(e.target.value);
+  };
 
   return (
-    <ImagesContext.Provider value={{ photos }}>
-        {props.children}
+    <ImagesContext.Provider value={{ photos, images, query, SearchImages }}>
+      {props.children}
     </ImagesContext.Provider>
-)
-}
+  );
+};
 
-export default ImagesContextProvider
+export default ImagesContextProvider;
