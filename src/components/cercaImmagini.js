a1c3e972642dfd834bb2a4474df8bd0ab5a4f575
francescabrondi <37108063+francescabrondi@users.noreply.github.com>
2022-04-12 16:40:35 +0200

    Struttura modal (non funziona ancora)

--- src/components/cercaImmagini.js
+++ src/components/cercaImmagini.js
@@ -1,21 +1,30 @@
 import React, { useContext } from "react";
 import { ImagesContext } from "../contexts/ImmaginiContext";
+import LoadingSpinner from "./LoadingSpinner";
 
 function Cercaimmagini() {
-  const { images, query, SearchImages } = useContext(ImagesContext);
+  const { images, query, SearchImages, isLoading, reset } =
+    useContext(ImagesContext);
 
   return (
     <main className="immagini page">
       <h1>Ricerca immagini</h1>
       <input type="text" value={query} onChange={SearchImages} />
-      <ul>
-        {images.length > 0 &&
-          images.map((photo) => (
-            <li key={photo.id}>
-              <img src={photo.urls.regular} alt="" />
-            </li>
-          ))}
-      </ul>
+      <input type="submit" value="Reset" onClick={reset} />
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <ul>
+          {images.length > 0 &&
+            images.map((photo) =>
+              ""(
+                <li key={photo.id}>
+                  <img src={photo.urls.regular} alt="" />
+                </li>
+              )
+            )}
+        </ul>
+      )}
     </main>
   );
 }
